61398613983d8fcf1695bc058115a14713e1b79e
santinucera <santinucera4@gmail.com>
2017-09-11 00:17:44 -0300

    Agrego algunos tipos

--- BibliotecasCompartidas/Globales.h
+++ BibliotecasCompartidas/Globales.h
@@ -8,6 +8,42 @@
 #ifndef GLOBALES_H_
 #define GLOBALES_H_
 
+#include <stdbool.h>
 
+typedef struct respuestaTransformacion{
+	int nodo;
+	char* ip;
+	int puerto;
+	int bloque;
+	int byteOcupado;
+	char* rutaArchivoTemp;
+};
+
+typedef struct respuestaReduccionLocal{
+	int nodo;
+	char* ip;
+	int puerto;
+	char* archivoTransformacion;
+	char* archivoReduccion;
+};
+
+typedef struct respuestaReduccionGlobal{
+	int nodo;
+	char* ip;
+	int puerto;
+	char* archivoReduccionLocal;
+	char* archivoReduccionGlobal;
+	bool encargado;
+};
+
+typedef struct registroTablaEstados{
+	int job;
+	int master;
+	int nodo;
+	int bloque;
+	int estapa;
+	char* rutaArchivoTemp;
+	int estado;
+};
 
 #endif /* GLOBALES_H_ */
